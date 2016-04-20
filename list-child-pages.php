adab310350b248c80e9e13641812bfaec3a1da90
Stephanie Leary <steph@sillybean.net>
2016-04-20 16:03:11 -0500

    bump

--- list-child-pages.php
+++ list-child-pages.php
@@ -7,6 +7,7 @@ GitHub Branch: master
 Description: Lets you list child pages using a shortcode. Also displays child page list by default on empty parent pages.
 Author: Stephanie Leary
 Version: 1.2
+Tested up to: 4.5
 Author URI: http://stephanieleary.com/
 License: GPL v2 or later
 */ 
