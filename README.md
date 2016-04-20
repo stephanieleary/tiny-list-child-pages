22081cb1701b579bcd00129f66f992c4b82c2f7e
Stephanie Leary <steph@sillybean.net>
2016-04-20 15:54:52 -0500

    github updater support

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# tiny-list-child-pages
-Lets you list child pages using a shortcode. Also displays child page list by default on empty parent pages.
+=== Stephanie's Tiny List Child Pages Plugin ===
+Plugin URI: http://stephanieleary.com/2010/06/listing-child-pages-with-a-shortcode/
+GitHub Plugin URI: https://github.com/sillybean/tiny-list-child-pages
+GitHub Branch: master
+Description: Lets you list child pages using a shortcode. Also displays child page list by default on empty parent pages.
+Author: Stephanie Leary
+Version: 1.2
+Author URI: http://stephanieleary.com/
+License: GPL v2 or later
--- list-child-pages.php
+++ list-child-pages.php
@@ -1,12 +1,14 @@
 <?php
 /*
-	Plugin Name: Stephanie's Tiny List Child Pages Plugin
-	Plugin URI: http://stephanieleary.com/2010/06/listing-child-pages-with-a-shortcode/
-	Description: Lets you list child pages using a shortcode. Also displays child page list by default on empty parent pages.
-	Author: Stephanie Leary
-	Version: 1.2
-	Author URI: http://stephanieleary.com/
-	License: GPL v2 or later
+Plugin Name: Stephanie's Tiny List Child Pages Plugin
+Plugin URI: http://stephanieleary.com/2010/06/listing-child-pages-with-a-shortcode/
+GitHub Plugin URI: https://github.com/sillybean/tiny-list-child-pages
+GitHub Branch: master
+Description: Lets you list child pages using a shortcode. Also displays child page list by default on empty parent pages.
+Author: Stephanie Leary
+Version: 1.2
+Author URI: http://stephanieleary.com/
+License: GPL v2 or later
 */ 
 
 function scl_child_pages_shortcode() {
@@ -15,11 +17,9 @@ function scl_child_pages_shortcode() {
 add_shortcode( 'child-pages', 'scl_child_pages_shortcode' );
 add_shortcode( 'subpages', 'scl_child_pages_shortcode' );
 
-
 function scl_append_child_pages( $content ) {
-	
    $type = get_post_type();
-   if ( is_post_type_hierarchical( $type )  && empty( $content) ) {
+   if ( is_post_type_hierarchical( $type )  && empty( $content ) ) {
       $args = array(
       	'echo' => 0,
       	'depth' => 1,
